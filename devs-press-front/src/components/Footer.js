f524301b7444838276c93de118c06c2eb8fab736
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-09-10 15:02:31 +0300

    add footer

--- devs-press-front/src/components/Footer.js
+++ devs-press-front/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return (
         <div style={{padding: 40+"px", backgroundColor:"black", color:"white"}}>
-            <h6 style={{textAlign:"center"}}>Designed By Judy Mwai</h6>
+            <h6 style={{textAlign:"center"}}>Designed By Byron Odhiambo</h6>
         </div>
     )
 }
