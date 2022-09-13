7edda459d6a5566f1a00dfd85a357b2c793b7a85
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-09-14 01:43:00 +0300

    esit  homepagein package.json

---
+++ build/precache-manifest.e991493253e30d02cf0a25f933b73be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11f13bc370bed735f8cd",
+    "url": "/Devspress/static/css/main.995c42d7.chunk.css"
+  },
+  {
+    "revision": "11f13bc370bed735f8cd",
+    "url": "/Devspress/static/js/main.11f13bc3.chunk.js"
+  },
+  {
+    "revision": "eace23341504799fa3de",
+    "url": "/Devspress/static/js/1.eace2334.chunk.js"
+  },
+  {
+    "revision": "bce9a3767db3ddfbbf78",
+    "url": "/Devspress/static/js/runtime~main.bce9a376.js"
+  },
+  {
+    "revision": "a5a271236e3eabd1476bd359e6c31687",
+    "url": "/Devspress/index.html"
+  }
+];
