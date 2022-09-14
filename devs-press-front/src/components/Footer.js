3e19f075a3ad92d90a266e589f439a221d7b68e4
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-09-14 12:56:50 +0300

    change footer background color

--- devs-press-front/src/components/Footer.js
+++ devs-press-front/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(){
     return (
-        <div style={{padding: 40+"px", backgroundColor:"black", color:"white"}}>
+        <div style={{padding: 40+"px", backgroundColor:"#264248", color:"white"}}>
             <h6 style={{textAlign:"center"}}>Designed By Byron Odhiambo</h6>
         </div>
     )
